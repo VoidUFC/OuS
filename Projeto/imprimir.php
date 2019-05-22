25a559a413cad3bc5346cf5c3257f06a64693a2c
VoidUFC <49084042+VoidUFC@users.noreply.github.com>
2019-05-22 20:58:31 -0300

    Update imprimir.php

--- Projeto/imprimir.php
+++ Projeto/imprimir.php
@@ -50,7 +50,7 @@
 <head>        
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />              
-    <title>OS-12432</title>
+    <title>OS-<?php echo $os_numero?></title>
     <link rel="icon" type="image/ico" href="favicon.ico"/>
     <link href="css/stylesheets.css" rel="stylesheet" type="text/css" />        
     <script type='text/javascript' src='js/jquery/jquery.min.js'></script>
